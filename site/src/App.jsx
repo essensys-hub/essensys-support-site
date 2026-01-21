0ff66eb13903121ac599b3cb4580e5583444edc9
rhinosys <nicolas.rineau@rhinosys.fr>
2026-01-21 23:24:49 +0100

    modifiy home page

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -25,7 +25,6 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/privacy" element={<Privacy />} />
-          <Route path="/catalog" element={<Catalog />} />
           <Route
             path="ios"
             element={
