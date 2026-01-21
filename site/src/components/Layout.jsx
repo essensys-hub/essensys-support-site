2a7ad52c6dff58775dda73811a07893b1337d10b
rhinosys <nicolas.rineau@rhinosys.fr>
2026-01-21 23:28:42 +0100

    modifiy home page

--- site/src/components/Layout.jsx
+++ site/src/components/Layout.jsx
@@ -33,7 +33,6 @@ const Layout = ({ children }) => {
                     <ul>
                         <li><Link to="/">Accueil</Link></li>
                         <li><Link to="/support">Support</Link></li>
-                        <li><Link to="/catalog">Catalogue</Link></li>
                         <li><Link to="/raspberrypi">Raspberry Pi</Link></li>
                         {['admin_global', 'admin_local', 'admin'].includes(adminRole) && (
                             <li><Link to="/admin">Admin</Link></li>
